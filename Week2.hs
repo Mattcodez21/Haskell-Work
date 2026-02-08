cf0626d3593eda099baa9298515bb73badb80670
Mattcodez21 <166388565+Mattcodez21@users.noreply.github.com>
2026-02-08 15:00:52 +0000

    Worked Examples & Question 1

---
+++ Week2.hs
@@ -0,0 +1,31 @@
+-- Worked Example 1:
+
+heartMonitor :: Int -> Int -> String
+heartMonitor age bpm
+  | age > 80  && bpm > 100 = "High heart rate for 81+!"
+  | age > 60  && bpm > 130 = "High heart rate for 61-80!"
+  | age > 40  && bpm > 140 = "High heart rate for 41-60!"
+  | age > 20  && bpm > 155 = "High heart rate for 21-40!"
+  | age >= 0  && bpm > 170 = "High heart rate for 0-20!"
+  | otherwise              = "Normal heart rate"
+
+
+-- Worked Example 2:
+
+pizzaCalories :: Int -> String -> Float
+pizzaCalories diameter toppings = (11.5 + toppingCalories) * area
+  where
+    area = pi * (fromIntegral diameter / 2) ^ 2
+    
+    toppingCalories
+      | toppings == "pepperoni" = 6
+      | toppings == "tuna"      = 4
+      | toppings == "veggie"    = 2.5
+      | otherwise               = 0
+
+--1
+
+absolute :: Int -> Int 
+absolute x 
+  | x < 0 = -x 
+  | otherwise = x 
