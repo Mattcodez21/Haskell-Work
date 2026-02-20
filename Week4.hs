4da8c80f21cd3a37d6fe99ff78425bba307c2995
Mattcodez21 <166388565+Mattcodez21@users.noreply.github.com>
2026-02-20 20:36:00 +0000

    Q4

--- Week4.hs
+++ Week4.hs
@@ -91,3 +91,8 @@ capMark (name, mark)
   | mark < 0 || mark > 100 = error "Mark must be between 0 and 100"
   | mark > 40              = (name, 40)  -- Cap at 40
   | otherwise              = (name, mark)  -- Keep original
+
+--Q4: First Numbers:
+
+firstNumbers :: Int -> [Int]
+firstNumbers n = [1 .. n]
